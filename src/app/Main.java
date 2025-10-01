bba2fb963b0fae983fc131d9faaa065461447689
izanami <dargberimor@gmail.com>
2025-10-02 01:29:54 +0300

    Add fahrenheit to celsius

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,32 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("App for converting fahrenheit to celsius and vice versa");
+        byte choice  = getUserChoice();
+        System.out.println("enter the number which one to convert");
+        double number = scanner.nextDouble();
+        switch (choice) {
+            case 1:
+                System.out.println("here your celsius:" + String.format("%.3f",convFarToCel(number)));
+                break;
+            default:
+                System.out.println("Invalid choice");
+                break;
+        }
+    }
+
+    private static double convFarToCel(double fahrenheit) {
+        return ((fahrenheit - 32) * 5/9);
+    }
+
+
+    private static byte getUserChoice() {
+        System.out.println("Same converter, but now through SSH");
+        System.out.println("1:For fahrenheit to celsius");
+        return scanner.nextByte();
     }
 }
